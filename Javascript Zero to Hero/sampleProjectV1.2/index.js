54ac4137c04dfbd2f21e23a75942c6f03a5076a5
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2022-01-22 17:42:30 +0100

    m

--- Javascript Zero to Hero/sampleProjectV1.2/index.js
+++ Javascript Zero to Hero/sampleProjectV1.2/index.js
@@ -6,14 +6,25 @@ let textarea = document.getElementById('textarea');
 let posts_container = document.getElementById('posts_container');
 //we are now grabbing the form button by id value
 let btn = document.getElementById('form_btn');
+//getting the empty post message
+let emptyMsg  = document.getElementById('post');
+//create empty array to hold users posts
+let postsArray = [];
 
 
 form.addEventListener('submit', function(e){
   e.preventDefault();
   
+  postsArray.push(textarea.value);
+
+  if(postsArray.length !==0){
+    emptyMsg.style.display = 'none';
+  }
   let div = document.createElement('div');
   div.innerText = textarea.value;
-  
   posts_container.appendChild(div);
+
+  //making the textarea empty after posting
+  textarea.value = '';
 })
 
